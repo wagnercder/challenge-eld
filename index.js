91f7fed79c19a2d20c221bc080c61733a1424c8d
Wagner Queiroz <wagner.coder@gmail.com>
2020-04-26 16:05:32 -0300

    Count valid and unvalid answers

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ var dir = []
 
 //Get args
 process.argv.forEach((val, index) => {
-    console.log(`${index}: ${val}`)
-
-    if(index >= 2)
+    if(index >= 2){
         dir.push(val)
+        console.log(`${index}: ${val}`)
+    }
 })
 
 var file_array_content = []
-var survey_relation = []
+var valid_answers = []
+var invalid_answers = []
+var key_companie = []
 
 dir.forEach(file_dir => {
     file_array_content.push(fs.readFileSync(process.cwd() + file_dir, 'utf8').split("\n"))
 })
 
-console.log(file_array_content[0])
-
 var companies_relation = []
 
+//Separate every company survey into array and discard invalid answers
 file_array_content.forEach(company_data => {
     size = company_data.length
 
@@ -27,13 +28,22 @@ file_array_content.forEach(company_data => {
         if(i > 0){
             aux = company_data[i].split(" ")
 
-            if(parseInt(aux[1]) >= 0 && aux[1] <= 4)
+            if(parseInt(aux[1]) >= 0 && aux[1] <= 4){
                 companies_relation[company_data[0]].push(aux)
+                valid_answers[company_data[0]]++
+            } else
+                invalid_answers[company_data[0]]++
 
         }else{
             companies_relation[company_data[0]] = []
+            valid_answers[company_data[0]] = 0
+            invalid_answers[company_data[0]] = 0
+            key_companie.push(company_data[0])
         }
     }
 })
 
+console.log(valid_answers)
+console.log(invalid_answers)
 console.log(companies_relation)
+
