4cdd0a141d5557fad6d0e6c2806fdd03d5162727
Wagner Queiroz <wagner.coder@gmail.com>
2020-04-26 17:30:43 -0300

    Summary by companies added

--- index.js
+++ index.js
@@ -43,7 +43,43 @@ file_array_content.forEach(company_data => {
     }
 })
 
-console.log(valid_answers)
-console.log(invalid_answers)
+// console.log(valid_answers)
+// console.log(invalid_answers)
+var questions_keys = []
+
 console.log(companies_relation)
 
+key_companie.forEach( key => {
+    var questions = []
+    
+    companies_relation[key].forEach( relation => {
+        if(relation[0] in questions){ //count
+            questions[relation[0]]['total']++
+
+            if(relation[1] === '0' || relation[1] === '1')
+                questions[relation[0]]['fav']++
+            else if(relation[1] === '2')
+                questions[relation[0]]['neutral']++
+            else if(relation[1] === '3' || relation[1] === '4')
+                questions[relation[0]]['unfav']++
+
+        } else { //initialize
+            questions[relation[0]] = []
+            questions[relation[0]]['fav'] = relation[1] == '0' || relation[1] == '1' ? 1 : 0
+            questions[relation[0]]['neutral'] = relation[1] == '2' ? 1 : 0
+            questions[relation[0]]['unfav'] = relation[1] == '3' || relation[1] == '4' ? 1 : 0
+            questions[relation[0]]['total'] = 1
+
+            questions_keys.push(relation[0])
+        }
+    })
+
+    console.log(key)
+
+    questions_keys.forEach(key => {
+        console.log(key+': '+(questions[key]['fav'] * 100/questions[key]['total']).toFixed(2) +'% fav, ' +
+        (questions[key]['neutral'] * 100/questions[key]['total']).toFixed(2)+ '% neutral, '+
+        (questions[key]['unfav'] * 100/questions[key]['total']).toFixed(2)+ '% unfav ');
+    })
+    
+})
